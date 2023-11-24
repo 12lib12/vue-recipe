761ce59b9f431b00a7f4844db4daeed4ad60b51c
12lib12 <rangganabil2006@gmail.com>
2023-11-24 19:00:57 +0700

    recipe1

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Web-header></Web-header>
+    <router-view></router-view>
+    <Web-footer></Web-footer>
+  </div>
+</template>
+
+<script setup>
+import WebFooter from "./components/footer/WebFooter.vue";
+import WebHeader from "./components/header/WebHeader.vue";
+</script>
---
+++ src/components/header/NavigationBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div
+    class="header__navbar row
+    justify-content-between align-items-center"
+    style="width: 450px"
+    >
+    <search-menu></search-menu>
+    <signup-menu></signup-menu>
+    </div>
+</template>
+
+<script setup>
+import SearchMenu from './SearchMenu.vue'
+import SignupMenu from './SignupMenu.vue'
+
+</script>
